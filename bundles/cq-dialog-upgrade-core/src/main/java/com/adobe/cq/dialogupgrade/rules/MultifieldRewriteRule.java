ab376d75ec791dc73cfdfc4ee2b4f2ddde269972
Damien Obrist <obrist@adobe.com>
2014-11-28 17:41:28 +0100

    Improved / generalized multifield component

--- bundles/cq-dialog-upgrade-core/src/main/java/com/adobe/cq/dialogupgrade/rules/MultifieldRewriteRule.java
+++ bundles/cq-dialog-upgrade-core/src/main/java/com/adobe/cq/dialogupgrade/rules/MultifieldRewriteRule.java
@@ -2,6 +2,7 @@ package com.adobe.cq.dialogupgrade.rules;
 
 import com.adobe.cq.dialogupgrade.api.DialogRewriteRule;
 import com.adobe.cq.dialogupgrade.treerewriter.RewriteException;
+import com.day.cq.commons.jcr.JcrUtil;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
@@ -9,8 +10,6 @@ import org.apache.felix.scr.annotations.Service;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 import static com.adobe.cq.dialogupgrade.DialogUpgradeUtils.hasXtype;
@@ -25,15 +24,8 @@ import static com.adobe.cq.dialogupgrade.treerewriter.TreeRewriterUtils.renameTo
 public class MultifieldRewriteRule implements DialogRewriteRule {
 
     private static final String XTYPE = "multifield";
-    private static final String GRANITEUI_CHECKBOX_RT = "granite/ui/components/foundation/form/multifield";
-
-    private static final Map<String,String> FIELDCONFIG_MAPPINGS = new HashMap<String,String>();
-    static {
-        FIELDCONFIG_MAPPINGS.put("textfield", "granite/ui/components/foundation/form/textfield");
-        FIELDCONFIG_MAPPINGS.put("textarea", "granite/ui/components/foundation/form/textarea");
-        FIELDCONFIG_MAPPINGS.put("pathfield", "granite/ui/components/foundation/form/pathbrowser");
-        // todo: selection (type = combobox or select)
-    }
+    private static final String GRANITEUI_MULTIFIELD_RT = "granite/ui/components/foundation/form/multifield";
+    private static final String GRANITEUI_TEXTFIELD_RT = "granite/ui/components/foundation/form/textfield";
 
     public boolean matches(Node root)
             throws RepositoryException {
@@ -49,24 +41,21 @@ public class MultifieldRewriteRule implements DialogRewriteRule {
         // add node for multifield
         Node newRoot = parent.addNode(name, "nt:unstructured");
         finalNodes.add(newRoot);
-        newRoot.setProperty("sling:resourceType", GRANITEUI_CHECKBOX_RT);
+        newRoot.setProperty("sling:resourceType", GRANITEUI_MULTIFIELD_RT);
         // set properties
         copyProperty(root, "fieldLabel", newRoot, "fieldLabel");
         copyProperty(root, "fieldDescription", newRoot, "fieldDescription");
 
-        // add 'field' subnode
-        Node field = newRoot.addNode("field", "nt:unstructured");
-        finalNodes.add(field);
-        // use textfield as the default resource type
-        String resourceType = FIELDCONFIG_MAPPINGS.get("textfield");
-        if (root.hasProperty("fieldConfig/xtype")) {
-            String xtype = root.getProperty("fieldConfig/xtype").getString();
-            if (FIELDCONFIG_MAPPINGS.containsKey(xtype)) {
-                resourceType = FIELDCONFIG_MAPPINGS.get(xtype);
-            }
+        Node field;
+        if (root.hasNode("fieldConfig")) {
+            field = JcrUtil.copy(root.getNode("fieldConfig"), newRoot, "field");
+            field.setPrimaryType("cq:Widget");
+            copyProperty(root, "name", field, "name");
+        } else {
+            field = newRoot.addNode("field", "nt:unstructured");
+            finalNodes.add(field);
+            field.setProperty("sling:resourceType", GRANITEUI_TEXTFIELD_RT);
         }
-        field.setProperty("sling:resourceType", resourceType);
-        copyProperty(root, "name", field, "name");
 
         // remove old root and return new root
         root.remove();
